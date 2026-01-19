03e0a596b4156a697e9952af8a09f5780dcdf3db
reconandreel-stack <reconandreel@gmail.com>
2026-01-19 12:01:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 
 const CONFIG = {
   limit: 100,
-  webhookUrl: "https://script.google.com/macros/s/AKfycbwDVK8pRhF0-TnkaiS7Kdsw1nJ-xAcjBCMn6k_k6ehQm6MLjxRVsxc00qCXu3I767UL/exec", // 
+  webhookUrl: "https://script.google.com/macros/s/AKfycbwDVK8pRhF0-TnkaiS7Kdsw1nJ-xAcjBCMn6k_k6ehQm6MLjxRVsxc00qCXu3I767UL/exec",  
 
 const $ = (id) => document.getElementById(id);
 
